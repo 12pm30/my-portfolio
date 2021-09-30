ca47712c1366f0783fc87695f62eb7e3a19fa897
parv.mital@vts.com <pmital-vts@vts.com>
2021-09-30 13:31:13 -0400

    Updates

---
+++ precache-manifest.964020b936c27754f6bcdc9bfedf297f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfd60105e625b6b6aee8",
+    "url": "/static/css/main.7a21227f.chunk.css"
+  },
+  {
+    "revision": "bfd60105e625b6b6aee8",
+    "url": "/static/js/main.bfd60105.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7a715afa694027913706e1a0f1f0a71",
+    "url": "/index.html"
+  }
+];
